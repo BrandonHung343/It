773e5b95f0c371fe9fd0ed6c8fed092971c6366a
joshuadurham <jbdurham@andrew.cmu.edu>
2017-09-08 20:58:30 -0400

    Product Class Added

---
+++ app/src/main/java/com/example/wurf/Product.java
@@ -0,0 +1,27 @@
+package com.example.wurf;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by joshDurham on 9/8/17.
+ */
+
+public class Product {
+
+    String productName;
+    int productId;
+    String producer;
+    int producerId;
+    String market;
+    int marketId;
+    String submarket;
+    int submarketId;
+    double price;
+    int elo;
+    String description;
+    Bitmap image;
+    
+    public Product(String productName, int productId, String producer, int producerId, String market, int marketId, String submarket, int submarketId, double price, int elo, String description, Bitmap image){
+
+    }
+}
