becf23555d193313ace96892039e69ba3a775b13
joshuadurham <jbdurham@andrew.cmu.edu>
2017-09-08 21:11:36 -0400

    Product Class Added2

--- app/src/main/java/com/example/wurf/Product.java
+++ app/src/main/java/com/example/wurf/Product.java
@@ -22,6 +22,37 @@ public class Product {
     Bitmap image;
     
     public Product(String productName, int productId, String producer, int producerId, String market, int marketId, String submarket, int submarketId, double price, int elo, String description, Bitmap image){
-
+        productName = this.productName;
+        productId = this.productId;
+        producer = this.producer;
+        producerId = this.producerId;
+        market = this.market;
+        marketId = this.marketId;
+        submarket = this.submarket;
+        submarketId = this.submarketId;
+        price = this.price;
+        elo = this.elo;
+        description = this.description;
+        image = this.image;
     }
+
+    public String getProductName(){
+        return productName;
+    }
+
+    public int getProductId(){
+        return productId;
+    }
+
+    public String getProducer(){
+        return producer;
+    }
+
+    public int getProducerId(){
+        return producerId;
+    }
+
+    public
+
+
 }
